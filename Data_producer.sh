1ad75c7a831b7fb9f2dcddf711568f81e985f0d2
personal_user <personal_email>
2024-04-18 00:30:54 -0400

    BUUUUGS!!!!!

--- Data_producer.sh
+++ Data_producer.sh
@@ -7,12 +7,13 @@ if [ -p /tmp/Zepipe01 ]; then
     rm /tmp/Zepipe01
 fi
 
-# Create a new pipe
-mkfifo /tmp/Zepipe01
+# Create a pipe
+pipe_fd="/tmp/Zepipe01"
+mkfifo $pipe_fd
 
 # Prompt the user for input
 echo "Enter a message:"
 read message
 
 # Call the C function from the shared library to write to the pipe
-./libpipe_functions.so "/tmp/Zepipe01" "$message"
+./libpipe_functions.so "$pipe_fd" "$message"
--- pipe_functions.c
+++ pipe_functions.c
@@ -13,11 +13,10 @@ __attribute__((visibility("default"))) void writeToPipe(int pipe_fd, const char*
 
 // Function to read a message from a pipe
 __attribute__((visibility("default"))) void readFromPipe(int pipe_fd, char* buffer, size_t buffer_size) {
-    ssize_t bytes_read = read(pipe_fd, buffer, buffer_size);
+    ssize_t bytes_read = read(pipe_fd, buffer, buffer_size - 1); // Ensure there's room for null terminator
     if (bytes_read == -1) {
         perror("Error reading from pipe");
         // Handle error (e.g., return or exit)
     }
-    // Null-terminate the buffer to ensure it's a valid string
-    buffer[bytes_read] = '\0';
+    buffer[bytes_read] = '\0'; // Null-terminate the buffer
 }
